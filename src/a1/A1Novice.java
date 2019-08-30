d851906095bfb9ff617b8e2c35edce9becf6934c
alextoney22 <54328728+alextoney22@users.noreply.github.com>
2019-08-30 11:42:42 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
 		Scanner scan = new Scanner(System.in);
