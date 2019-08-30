e9586380285a057f4b44ed35c89f22762eae704d
alextoney22 <54328728+alextoney22@users.noreply.github.com>
2019-08-30 10:20:35 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,69 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		Scanner scan = new Scanner(System.in);
+
+		
+		// Variable for number of customers
+		int num_of_customers = scan.nextInt();
+		int customers_nums = 0;
+		
+		// Variables for first name and last name
+		String first_name = "";
+		String last_name = "";
+		
+		// Variables for number of items bought
+		int num_of_items = 0;
+		int num_bought = 0;
+		
+		// Variables for item cost
+		double item_cost = 0.0;
+		
+		// Total Cost of items
+			double total_cost = 0.0;
+		
+		// Array for customers
+		String customers[];
+		customers = new String[customers_nums];
+		
+		for(int i = 0; i < num_of_customers; i++)
+		{
+			
+			
+			// Scanning first name and taking first character then last name and number of items
+			first_name = Character.toString(scan.next().charAt(0));
+			last_name = scan.next();
+			num_of_items = scan.nextInt();
+			
+			for(int j = 0; j < num_of_items; j++)
+			{
+				// Number of items bought by the customer
+				num_bought = scan.nextInt();
+				
+				// Name of items 
+				scan.next();
+				
+				// Item cost
+				item_cost = scan.nextDouble();
+				
+				// Adding together cost of items
+				total_cost += item_cost;
+				
+			}
+			
+			customers[i] = first_name + ". " + last_name + " " + String.format("%.2f", total_cost);
+			
+			
+		}	
+		
+		// Print everything
+		for ( int i = 0; i < customers.length; i ++)
+		{
+			System.out.println(customers);
+		}
+		
+		scan.close();
+	
 		
 	}
 }
